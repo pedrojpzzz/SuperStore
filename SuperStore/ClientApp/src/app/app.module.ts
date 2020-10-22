6addb1ae7281aa5a7aaa93b8ace9c774e2fd6422
pedrojpzzz <pedrojpzzz@gmail.com>
2020-10-22 23:11:21 +0200

    delete old cart and example components

--- SuperStore/ClientApp/src/app/app.module.ts
+++ SuperStore/ClientApp/src/app/app.module.ts
@@ -7,10 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
@@ -18,10 +15,7 @@ import { CartComponent } from './cart/cart.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
     ProductListComponent,
-    ShoppingCartComponent,
-    FetchDataComponent,
     CartComponent
   ],
   imports: [
@@ -30,10 +24,7 @@ import { CartComponent } from './cart/cart.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
       { path: 'product-list', component: ProductListComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'cart', component: CartComponent }
     ])
   ],
